17ed9fd37b32e4adcc36ee1d60e2d37d528dfa57
kim-min-gyu136 <70559480+kim-min-gyu136@users.noreply.github.com>
2020-12-11 05:52:30 +0900

    수정 upload

---
+++ code/ch08/nullpointer.c
@@ -0,0 +1,16 @@
+//file : nullpointer.c
+
+#include<stdio.h>
+
+int main(void)
+{
+	int *ptr1, *ptr2, data = 10;
+	ptr1 = NULL;
+
+	printf("%p\n", ptr1);
+	//printf("%p\n",ptr2) 초기값이 없어서 컴파일 오류발생
+	printf("%d\n", data);
+
+
+	return 0;
+}
